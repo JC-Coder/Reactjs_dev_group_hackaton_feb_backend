5ff2745386449b3a4133a015b81c12137eb2ce98
JC-Coder <josephchimezie2003@gmail.com>
2023-02-18 20:17:49 +0100

    updated mongodb connection and removed vercel file

--- connection.js
+++ connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(`mongodb://localhost:27017/hackaton_01`, () => {
+const CONNECTION_STRING = process.env.MONGODB_URL_LIVE
+
+mongoose.connect(CONNECTION_STRING, () => {
     console.log('connected to mongodb');
 }); 
